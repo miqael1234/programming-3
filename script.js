54c2554f0963f42f0b751b0c8dc4eb862ce2ef04
sargis12 <sargis.k5@tumo.org>
2023-10-21 17:23:59 +0400

    added new click

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ socket = io();
 var side = 60;
 var n = 15;
 var m = 15;
+var flag = false;
 
 
 function setup() {
@@ -12,11 +13,20 @@ function setup() {
 
 function drawGame(matrix) {
 
+    var tumo = document.getElementById("tumo");
+    tumo.addEventListener("click", clickHandler, false);
+    tumo.myParam = matrix;
+
+
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
 
-            if (matrix[y][x] == 1) {
+            if (matrix[y][x] == 1 && flag == false) {
                 fill("green");
+            }else if(matrix[y][x] == 1 && flag == true){
+                fill('black');
+                text('Tumo', x * side, y * side)
+                fill("blue");
             }
             else if (matrix[y][x] == 0) {
                 fill("#acacac");
@@ -26,13 +36,35 @@ function drawGame(matrix) {
             }
             else if (matrix[y][x] == 3) {
                 fill("red");
+
             }
             rect(x * side, y * side, side, side);
         }
-
     }
-
 }
 
-
 socket.on('matrix', drawGame)
+
+
+
+function clickHandler(evt){
+    matrix = evt.currentTarget.myParam;
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                flag = true;
+                fill('black');
+                text('Tumo', x * side, y * side)
+                fill("blue");
+            }
+        }
+    }
+}
+
+var clean = document.getElementById("clean");
+clean.addEventListener("click", clickHandlerClean, false);
+
+function clickHandlerClean(evt){
+    flag = false;
+}
